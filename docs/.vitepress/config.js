4195cda519255afd6936cfb27626ed3dc361c244
mzj <3424126130@qq.com>
2026-04-13 22:27:13 +0800

    初始化

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,48 @@
+export default {
+  base: process.env.GITHUB_ACTIONS ? "/mzj-note/" : "/",
+  title: "蘑菇",
+  description: "我的个人博客",
+  head: [["link", { rel: "icon", href: "/img/mushroom.png" }]],
+  themeConfig: {
+    logo: "/img/logo.png",
+    nav: [{ text: "首页", link: "/" }],
+    search: {
+      provider: "local",
+    },
+    outline: {
+      label: "页面导航",
+      level: [2, 3],
+    },
+    sidebar: [
+      {
+        text: "待添加",
+        collapsed: true,
+        items: [],
+      },
+      {
+        text: "前端相关",
+        collapsed: true,
+        items: [
+          {
+            text: "组件间调用",
+            link: "/notes/guide/前端相关/组件调用",
+          },
+        ],
+      },
+      {
+        text: "随心记",
+        collapsed: true,
+        items: [
+          {
+            text: "Markdown 常用语法",
+            link: "/notes/guide/随心记/Markdown常用语法",
+          },
+          {
+            text: "属性解释",
+            link: "/notes/guide/随心记/属性解释",
+          },
+        ],
+      },
+    ],
+  },
+};
